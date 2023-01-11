ef27d77202999c2838c8e39e97d7c7271bda3029
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-01-11 10:55:29 -0300

    refactor: Ajustes no texto de erro e add da pasta assets

--- main.js
+++ main.js
@@ -10,14 +10,17 @@ function checkErros(form){
     var erros = []
     const regexNum = /[0-9]/
     const regexLetras = /[A-Za-z]/
-    if(form.senha.value.length < 6){
-        erros.push("A senha deve conter no mínimo de 6 caracteres")
+    if((!regexNum.test(form.senha.value) || !regexLetras.test(form.senha.value)) && form.senha.value.length < 6 ) {
+        erros.push("A senha deve conter letras, números e o mínimo de 6 caracteres.")
     }
-    if(!regexNum.test(form.senha.value) || !regexLetras.test(form.senha.value)) {
-        erros.push("A senha deve conter letras e números")
+    else if(form.senha.value.length < 6){
+        erros.push("A senha deve conter no mínimo de 6 caracteres.")
     }
-    if(!form.email.value.includes('@') || form.email.value.length < 3) {
-        erros.push('Por favor, insira um email válido');
+    else if(!regexNum.test(form.senha.value) || !regexLetras.test(form.senha.value)) {
+        erros.push("A senha deve conter letras e números.")
+    }
+    else if(!form.email.value.includes('@') || form.email.value.length < 3) {
+        erros.push('Por favor, insira um email válido.');
     }
     return erros
 }
