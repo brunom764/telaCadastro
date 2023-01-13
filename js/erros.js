4c404aa340ce192d102402638b59450cb027d595
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-01-13 08:59:02 -0300

    refactor:Ajuste na organização do codigo

--- js/erros.js
+++ js/erros.js
@@ -2,17 +2,17 @@ function checkErros(form){
     var erros = []
     const regexNum = /[0-9]/
     const regexLetras = /[A-Za-z]/
+    if(!form.email.value.includes('@') || form.email.value.length < 3) {
+        erros.push('Por favor, insira um email válido');
+    }
     if((!regexNum.test(form.senha.value) || !regexLetras.test(form.senha.value)) && form.senha.value.length < 6 ) {
         erros.push("A senha deve conter letras, números e o mínimo de 6 caracteres.")
     }
     else if(form.senha.value.length < 6){
-        erros.push("A senha deve conter no mínimo de 6 caracteres.")
+        erros.push("A senha deve conter no mínimo 6 caracteres.")
     }
     else if(!regexNum.test(form.senha.value) || !regexLetras.test(form.senha.value)) {
         erros.push("A senha deve conter letras e números.")
-    }
-    if(!form.email.value.includes('@') || form.email.value.length < 3) {
-        erros.push('Por favor, insira um email válido.');
     }
     return erros
 }
